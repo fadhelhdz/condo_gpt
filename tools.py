dd95b09be3e56da8b8662a113db8bf7d304a8479
fadhelhdz <hariz.dz97@gmail.com>
2024-11-27 10:28:04 +0900

    implement gmaps api

--- tools.py
+++ tools.py
@@ -1,16 +1,73 @@
+import os
 import re
 import ast
+
+from pydantic import BaseModel, Field
 
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
 from langchain.agents.agent_toolkits import create_retriever_tool
 from langchain_community.agent_toolkits import SQLDatabaseToolkit
+from langchain_community.tools import GooglePlacesTool
+from langchain.tools import BaseTool
+
+from googlemaps import Client as GoogleMaps
+
+google_places = GooglePlacesTool()
+gmaps = GoogleMaps(os.getenv("GPLACES_API_KEY"))
+
+class GeoCodingTool(BaseTool):
+    name = "google_maps_geocoding"
+    description = "Useful for converting addresses into geographic coordinates"
+    # args_schema = DirectionsInput
+
+    def _run(self, address):
+        try:
+            geocode_result = gmaps.geocode(address)
+            if geocode_result:
+                location = geocode_result[0]["geometry"]["location"]
+                print("--- GEOCODING TOOL ---")
+                print(address)
+                print(location)
+                print("--- GEOCODING TOO END ---")
+                return f'The coordinates for the address {address} are {location["lat"]}, {location["lng"]}.'
+            return "Unable to find the coordinates for the specific address."
+        except Exception as e:
+            return f"An error occured while fetching coordinates: {str(e)}"
+
+class DirectionsInput(BaseModel):
+    origin: str = Field(..., description="The starting point address or coordinates")
+    destination: str = Field(..., description="The destination point or coordinates")
+
+class DirectionsTool(BaseTool):
+    name = "google_maps_directions"
+    description = "Useful for finding travel distances and directions between two locations"
+    args_schema = DirectionsInput
+
+    def _run(self, origin, destination):
+        try:
+            print("DIRECTIONS API: ", origin, destination)
+            directions_result = gmaps.directions(origin, destination, mode="driving")
+            if directions_result:
+                distance = directions_result[0]["legs"][0]["distance"]["text"]
+                duration = directions_result[0]["legs"][0]["duration"]["text"]
+                return f"The travel distance from {origin} to {destination} is {distance}, and it takes approximately {duration} by car"
+            return "Unable to find directions between the specified locations."
+        except Exception as e:
+            return f"An error occured while fetching directions: {str(e)}"
+        
+directions_tool = DirectionsTool()
+google_maps_geocoding = GeoCodingTool()
+
+# def query_as_list(db, query):
+#     res = db.run(query)
+#     return ast.literal_eval(res)[0]
 
 def query_as_list(db, query):
     res = db.run(query)
-    return ast.literal_eval(res)[0]
-    
-
+    res = [el for sub in ast.literal_eval(res) for el in sub if el]
+    res = [re.sub(r"\b\d+\b", "", string).strip() for string in res]
+    return list(set(res))
 
 def setup_tools(db, llm):
     addresses = query_as_list(db, "SELECT address FROM core_condobuilding")
@@ -25,5 +82,7 @@ def setup_tools(db, llm):
     toolkit = SQLDatabaseToolkit(db=db, llm=llm)
     tools = toolkit.get_tools()
     tools.append(retriever_tool)
+    tools.append(google_maps_geocoding)
+    tools.append(directions_tool)
 
     return tools
